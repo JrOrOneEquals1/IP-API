bad11ca6a923a3254c21b8ffdc0057f01f9fe011
JrOrOneEquals1 <cdr5of8@gmail.com>
2023-09-21 16:24:37 -0600

    remove duped return line

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-# Flask, to receive the request
+# Flask, to receive original request
 from flask import Flask, request
-# Json, to parse str to obj
+# Json, to parse str to dict
 import json
-# Requests, to send requests to the API and get data
+# Requests, to send requests to the API and get data back
 import requests
 
 # Init the app
@@ -16,20 +16,21 @@ def result():
     data = json.loads(request.data)
     # Get type of value passed in (ip/domain/hash) case insensitive
     contentType = data['type'].lower()
-    # Get value
+    # Get value passed in, case sensitive
     content = data['content']
     # If type is ip or domain
     if contentType in ['ip', 'domain']:
         # Send request to WhoIs API to get info about ip or domain
         info = requests.post(f'https://www.whoisxmlapi.com/whoisserver/WhoisService?apiKey=at_sKqg6zX9HGSFXMrIXU4l8DwVTnE2C&domainName={content}&outputFormat=JSON').text
-        # Return the retrieved info as dict
-        return json.loads(info)
-    # If type is hash
-    if contentType == 'hash':
+    # If type is a file hash
+    elif contentType == 'hash':
         # Send request to VirusTotal API to get info about the hash
         info = requests.get(f'https://www.virustotal.com/api/v3/files/{content}', headers={'x-apikey': 'a9442725b4f377c0d308c043adf6ced518728174368f0d32b16a53fba621af4b'}).text
-        # Return the retrieved info as dict
-        return json.loads(info)
+    else:
+        # If type isn't ip/domain/hash
+        return f"Invalid type passed in: '{contentType}'"
+    # Return the retrieved info as dict
+    return json.loads(info)
 
 # Start the flask app
 app.run()
